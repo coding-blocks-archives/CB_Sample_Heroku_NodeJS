abe666f6f2add9bd17a6fad7050fd01622d7629e
Arnav Gupta <arnav@codingblocks.com>
2019-11-17 18:04:25 +0530

    add db to heroku
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ db.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+
+let db;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+  db = new Sequelize(process.env.CLEARDB_DATABASE_URL)
+} else {
+  db = new Sequelize({
+    host: 'localhost',
+    database: 'testdb',
+    username: '...',
+    password: '...'
+  })
+}
+
+const Tasks = db.define('tasks', {
+  name: Sequelize.STRING
+})
+
+module.exports = {
+  db, Tasks
+}
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const app = require('express')()
-
+const { Tasks, db } = require('./db')
 const PORT = process.env.PORT || 5555
 
-app.get('/', (req, res) => {
-  res.send("Hello Heroku!")
+app.get('/addtask', async (req, res) => {
+  await Tasks.create({ name: req.query.task })
+  res.redirect('/tasks')
 })
 
-app.listen(PORT)
+app.get('/tasks', (req, res) => {
+  const tasks =  await Tasks.findAll()
+  res.send(tasks.map(t => ` task = ${t.name} \n`))
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello Heroku!')
+})
+
+db.sync().then(() => {
+  app.listen(PORT)
+})
